cf195f57bba37eade63ccdc295fd1d6e2c138fad
Mohamed Elshaarawy <mohamed.elshaarawy@technivance.com>
2019-04-06 12:53:03 +0200

    feat: a little change

--- app/src/main/java/dev/elshaarawy/moviesii/MainActivity.kt
+++ app/src/main/java/dev/elshaarawy/moviesii/MainActivity.kt
@@ -1,28 +1,27 @@
 package dev.elshaarawy.moviesii
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_movies -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_like_dislike -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_info -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+        val stringId: Int? = when (item.itemId) {
+            R.id.navigation_movies -> R.string.title_home
+            R.id.navigation_like_dislike -> R.string.title_dashboard
+            R.id.navigation_info -> R.string.title_notifications
+            else -> null
         }
-        false
+        stringId?.let {
+            message.setText(it)
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            true
+        } ?: run {
+            false
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
